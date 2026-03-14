3ababd1e3b00c76a3a782ae2d246a04efa1f6844
Damian Socha <damian.socha@amigo.ie>
2026-03-14 09:55:00 +0000

    fixes for render1

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -83,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": config("DB_NAME", default="carenest"),
         "USER": config("DB_USER", default="carenest"),
-        "PASSWORD": config("DB_PASSWORD"),
+        "PASSWORD": config("DB_PASSWORD", default=""),
         "HOST": config("DB_HOST", default="localhost"),
         "PORT": config("DB_PORT", default="5432"),
     }
