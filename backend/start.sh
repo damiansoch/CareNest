8848f304e35bad29beb67e8064cc03f7c2f7d96f
Damian Socha <damian.socha@amigo.ie>
2026-03-14 10:35:20 +0000

    fixes for render4

--- backend/start.sh
+++ backend/start.sh
@@ -5,10 +5,10 @@ python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
 # Start Celery worker in background
-celery -A config worker -l info -Q default &
+# celery -A config worker -l info -Q default &
 
 # Start Celery beat in background
-celery -A config beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+# celery -A config beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start gunicorn in foreground (keeps container alive)
 exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2
